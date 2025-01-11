4db5fb0b9e8337057fbf9cc231350ffb774c620c
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-11 18:55:40 -0500

    Auto-commit on 2025-01-11T23:55:40.136Z

--- features/SplashText.js
+++ features/SplashText.js
@@ -1,3 +1,5 @@
+const FloydRegister = global.floyd.DynamicReload.FloydRegister;
+
 const obj = global.floyd.obj;
 
 const MainMenu = Java.type('net.minecraft.client.gui.GuiMainMenu');
@@ -5,7 +7,7 @@ const splashText = MainMenu.class.getDeclaredField('field_73975_c');
 
 splashText.setAccessible(true);
 
-register(net.minecraftforge.client.event.GuiScreenEvent.DrawScreenEvent.Pre, (event) => {
+FloydRegister(net.minecraftforge.client.event.GuiScreenEvent.DrawScreenEvent.Pre, (event) => {
    if(!(Client.currentGui.get() instanceof MainMenu) || !obj.splashText) return;
    splashText.set(Client.currentGui.get(), 'Floyd Client on top');
 });
