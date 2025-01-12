e466650287f1d8841bfa1a1636ec6b08a76bb1ca
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-12 14:30:17 -0500

    Auto-commit on 2025-01-12T19:30:17.480Z

--- features/SoundPlayer.js
+++ features/SoundPlayer.js
@@ -1,5 +1,5 @@
 const File = Java.type("java.io.File");
-const features = new File(`${Client.getMinecraft().field_71412_D.getPath()}/config/ChatTriggers/modules/Floyd/songs`)
+const features = new File(`${Client.getMinecraft().field_71412_D.getPath()}/config/ChatTriggers/modules/${global.floyd.getPath()}/songs`)
 const sounds = [];
 
 features.listFiles().forEach(file => {
@@ -8,7 +8,7 @@ features.listFiles().forEach(file => {
 })
 order.forEach(file => {
     try {
-        eval(FileLib.read(global.floyd.Path, `util/${file}.js`));
+        eval(FileLib.read(global.floyd.getPath(), `util/${file}.js`));
     } catch(err) {
         ChatLib.chat(`${file} ERROR!: ${err}`)
     }
