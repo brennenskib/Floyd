4fba64f9bb366230e0b9de98f26d7869e384fe9f
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-13 20:09:52 -0500

    Auto-commit on 2025-01-14T01:09:52.396Z

--- features/RetardTerms.js
+++ features/RetardTerms.js
@@ -1,5 +1,5 @@
 class RetardTerms {
-    function getNextSlot() {
+    colourSolver() {
         const allowedSlots = [10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 37, 38, 39, 40, 41, 42, 43];
         const replacements = { "light gray": "silver", "wool": "white", "bone": "white", "ink": "black", "lapis": "blue", "cocoa": "brown", "dandelion": "yellow", "rose": "red", "cactus": "green" };
         
