f0af1746aaa3fee3e11a2ba1bc7a4f1808a74791
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-14 21:50:06 -0500

    Auto-commit on 2025-01-15T02:50:06.003Z

--- features/WorldEdit.js
+++ features/WorldEdit.js
@@ -4,5 +4,5 @@ FileLib.write("Floyd-main", "WorldEdit.json", JSON.stringify(dat), true);
 register('packetSent', (packet, event) => {
     ChatLib.chat('test')
     let pos = new BlockPos(packet.func_179724_a());
-    ChatLib.command('ct copy ' + `{ x: ${pos.x}, y: ${pos.y}, z: ${pos.z}, blockType: ${World.getBlockAt(pos).type.getID()}`)
+    ChatLib.command('ct copy ' + `{ x: ${pos.x}, y: ${pos.y}, z: ${pos.z}, blockType: ${World.getBlockAt(pos).type.getID()}`, true)
 }).setPacketClass(net.minecraft.network.play.client.C08PacketPlayerBlockPlacement)
