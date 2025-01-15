c7b535d90f4b3556d9c42fc5fcc2bb2762c5473e
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-14 21:46:46 -0500

    Auto-commit on 2025-01-15T02:46:46.149Z

--- features/WorldEdit.js
+++ features/WorldEdit.js
@@ -1,3 +1,8 @@
 register('packetSent', (packet, event) => {
-    
+    let pos = new BlockPos(packet.func_179724_a());
+
+    let dat = {
+        pos: pos,
+        blockType: World.getBlockAt(pos).type.getID();
+    }
 }).setPacketClass(net.minecraft.network.play.client.C08PacketPlayerBlockPlacement)
