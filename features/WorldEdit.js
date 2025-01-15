0a290d25c231af11cf6a717a40e20e1841172a4b
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-14 21:47:56 -0500

    Auto-commit on 2025-01-15T02:47:56.231Z

--- features/WorldEdit.js
+++ features/WorldEdit.js
@@ -1,3 +1,6 @@
+const dat = JSON.parse(FileLib.read("Floyd-main", "WorldEdit.json")) ? JSON.parse(FileLib.read("Floyd-main", "WorldEdit.json")) : [];
+ 
+
 register('packetSent', (packet, event) => {
     let pos = new BlockPos(packet.func_179724_a());
 
