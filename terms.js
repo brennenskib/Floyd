38a1e2126982ebc5635bb84c0c4e747287395265
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-18 11:06:15 -0500

    Auto-commit on 2025-01-18T16:06:14.983Z

--- terms.js
+++ terms.js
@@ -7,7 +7,7 @@ class TerminalHandler {
 
         register('guiOpened', (event) => {
             this.windowId = Client.currentGui.get()
-            ChatLib.chat(this.windowId.class)
+            ChatLib.chat(this.windowId instanceof me.odinmain.features.impl.floor7.p3.termsim.TermSimGui)
         })
 
         register("tick", () => {
