4172d09034e64650717d82aaa70572ef41cd49bb
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-18 12:57:02 -0500

    Auto-commit on 2025-01-18T17:57:01.949Z

--- terms.js
+++ terms.js
@@ -33,7 +33,7 @@ class TerminalHandler {
             }
 
             if (Player.getContainer().getName().startsWith("Select all the ")) {
-                this.click(this.getColorIndex().toString());
+                this.click(this.getColorIndex());
             }
         })//.setFps(500)
     }
