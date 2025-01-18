3ad998aa4b992d72ee4e3a8b342003b147f1d7b0
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-18 13:18:09 -0500

    Auto-commit on 2025-01-18T18:18:09.597Z

--- terms.js
+++ terms.js
@@ -40,7 +40,7 @@ class TerminalHandler {
                     a.forEach((slot, index) => {
                         this.click(slot);
                         Thread.sleep(150 + (Math.random()*50))
-                        if(index == a.length-1) this.inTerminal = false;
+                        if(index == (a.length - 2)) this.inTerminal = false;
                         ChatLib.chat(this.inTerminal)
                     })
                 }).start()
