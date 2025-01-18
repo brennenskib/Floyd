542b9926f69f22d724178a4e86e177d9c30d2ee2
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-18 13:13:44 -0500

    Auto-commit on 2025-01-18T18:13:44.535Z

--- terms.js
+++ terms.js
@@ -16,7 +16,6 @@ class TerminalHandler {
 
         register('packetReceived', (p, e) => {
             this.windowId = p.func_148901_c(); // getWindowId
-            ChatLib.chat(this.windowId)
         }).setFilteredClass(S2DPacketOpenWindow);
 
         register("step", () => {
