ffff1fdd336cab0acb743bf9147c7cf564b91c7f
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-18 13:24:43 -0500

    Auto-commit on 2025-01-18T18:24:43.125Z

--- terms.js
+++ terms.js
@@ -28,7 +28,7 @@ class TerminalHandler {
 
                     a.forEach((slot, index) => {
                         this.click(slot);
-                        Thread.sleep(15)
+                        Thread.sleep(50)
                     })
                 }).start()
             }
