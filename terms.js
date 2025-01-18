4ec0cb6eb70a3ee9ca50dbafa11f33680886d605
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-18 18:09:46 -0500

    Auto-commit on 2025-01-18T23:09:46.828Z

--- terms.js
+++ terms.js
@@ -102,27 +102,28 @@ class TerminalHandler {
             } else if (iName == "Click the button on time!") {
                 this.inTerminal = true;
                 const s = new Thread(() => {
-                    while(this.inTerminal) {
-                        ChatLib.chat('test')
-                        let a = [9, 18, 27, 36]
-                        let slots = [16, 25, 34, 43]
-                        let i;
+                    ChatLib.chat('test')
+                    let a = [9, 18, 27, 36]
+                    let slots = [16, 25, 34, 43]
+                    let i;
         
-                        for (let index = 1; index < 5; index++) {
-                            let slot = Player.getContainer().getStackInSlot(index);
-                            if(!slot || slot.getDamage() !== 2) return;
-                            i = index;
-                        }
+                    for (let index = 1; index < 5; index++) {
+                        let slot = Player.getContainer().getStackInSlot(index);
+                        if(!slot || slot.getDamage() !== 2) return;
+                        i = index;
+                    }
         
-                        for (let index = 0; index < 4; index++) {
-                            let s = Player.getContainer().getStackInSlot(a[index] + (index+1))
-                            if(!s) return;
-                            if(s?.getDamage() == 5 && (index+1) == i) {
-                                this.click(slots[index])
-                            }
+                    for (let index = 0; index < 4; index++) {
+                        let s = Player.getContainer().getStackInSlot(a[index] + (index+1))
+                        if(!s) return;
+                        if(s?.getDamage() == 5 && (index+1) == i) {
+                            this.click(slots[index])
                         }
                     }
-                }).start()
+
+                    s.start()
+                })
+                s.start()
             }
         })
     }
