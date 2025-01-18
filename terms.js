ae158785e1ca0821227687c9b7d1f0f41419a725
brennenskib <179567099+brennenskib@users.noreply.github.com>
2025-01-18 13:43:56 -0500

    Auto-commit on 2025-01-18T18:43:56.693Z

--- terms.js
+++ terms.js
@@ -73,8 +73,10 @@ class TerminalHandler {
         let r = [];
 
         Player.getContainer().getItems().forEach((item, index) => {
-            if (ChatLib.removeFormatting(item?.getName()).startsWith(letter) && index < 44) this.correctPanes.push(index);
+            if (ChatLib.removeFormatting(item?.getName()).startsWith(letter) && index < 44) r.push(index);
         });
+
+        return r;
     }
     getColorIndex() {
         let color = Player.getContainer().getName().match(/Select all the (.+) items!/)[1].toLowerCase();
